9564a0950098afbd73ee7cc37ae7df4f24050428
DrIOS <58635327+DrIOSX@users.noreply.github.com>
2025-03-13 21:24:05 -0500

    Refactor Connect-TkMsService Unit Tests

--- tests/Unit/Private/Connect-TkMsService.tests.ps1
+++ tests/Unit/Private/Connect-TkMsService.tests.ps1
@@ -4,24 +4,87 @@ $ProjectName = ((Get-ChildItem -Path $ProjectPath\*\*.psd1).Where{
         $(try { Test-ModuleManifest $_.FullName -ErrorAction Stop } catch { $false } )
     }).BaseName
 
-
 Import-Module $ProjectName
 
 InModuleScope $ProjectName {
-    Describe Get-PrivateFunction {
-        Context 'Default' {
-            BeforeEach {
-                $return = Get-PrivateFunction -PrivateData 'string'
+    Describe 'Connect-TkMsService' {
+
+        # -- Mock your own function from your module:
+        Mock -CommandName 'Write-AuditLog' -ModuleName 'GraphAppToolkit'
+
+        # -- Microsoft.Graph commands:
+        Mock -CommandName 'Get-MgUser' -ModuleName 'Microsoft.Graph'
+        Mock -CommandName 'Get-MgContext' -ModuleName 'Microsoft.Graph'
+        Mock -CommandName 'Connect-MgGraph' -ModuleName 'Microsoft.Graph'
+        Mock -CommandName 'Remove-MgContext' -ModuleName 'Microsoft.Graph'
+        Mock -CommandName 'Get-MgOrganization' -ModuleName 'Microsoft.Graph'
+
+        # -- ExchangeOnlineManagement commands:
+        Mock -CommandName 'Get-OrganizationConfig' -ModuleName 'ExchangeOnlineManagement'
+        Mock -CommandName 'Connect-ExchangeOnline' -ModuleName 'ExchangeOnlineManagement'
+        Mock -CommandName 'Disconnect-ExchangeOnline' -ModuleName 'ExchangeOnlineManagement'
+
+        Context 'When connecting to Microsoft Graph' {
+
+            It 'Should connect to Microsoft Graph with specified scopes' {
+                $params = @{
+                    MgGraph         = $true
+                    GraphAuthScopes = @('User.Read', 'Mail.Read')
+                    Confirm         = $false
+                }
+                Connect-TkMsService @params
+
+                # Notice the -ModuleName arguments below, matching the mocks
+                Assert-MockCalled -CommandName 'Connect-MgGraph' -ModuleName 'Microsoft.Graph' -Times 1
+                Assert-MockCalled -CommandName 'Write-AuditLog' -ModuleName 'GraphAppToolkit' -Times 1 `
+                    -ParameterFilter { $_ -eq 'Connected to Microsoft Graph.' }
             }
 
-            It 'Returns a single object' {
-                ($return | Measure-Object).Count | Should -Be 1
+            It 'Should reuse existing Microsoft Graph session if valid' {
+                Mock -CommandName 'Get-MgUser' -ModuleName 'Microsoft.Graph' -MockWith { $null }
+                Mock -CommandName 'Get-MgContext' -ModuleName 'Microsoft.Graph' -MockWith { @{ Scopes = @('User.Read', 'Mail.Read') } }
+
+                $params = @{
+                    MgGraph         = $true
+                    GraphAuthScopes = @('User.Read', 'Mail.Read')
+                    Confirm         = $false
+                }
+                Connect-TkMsService @params
+
+                Assert-MockCalled -CommandName 'Get-MgUser' -ModuleName 'Microsoft.Graph' -Times 1
+                Assert-MockCalled -CommandName 'Write-AuditLog' -ModuleName 'GraphAppToolkit' -Times 1 `
+                    -ParameterFilter { $_ -eq 'An active Microsoft Graph session is detected and all required scopes are present.' }
+            }
+        }
+
+        Context 'When connecting to Exchange Online' {
+
+            It 'Should connect to Exchange Online' {
+                $params = @{
+                    ExchangeOnline = $true
+                    Confirm        = $false
+                }
+                Connect-TkMsService @params
+
+                Assert-MockCalled -CommandName 'Connect-ExchangeOnline' -ModuleName 'ExchangeOnlineManagement' -Times 1
+                Assert-MockCalled -CommandName 'Write-AuditLog' -ModuleName 'GraphAppToolkit' -Times 1 `
+                    -ParameterFilter { $_ -eq 'Connected to Exchange Online.' }
             }
 
-            It 'Returns a string based on the parameter PrivateData' {
-                $return | Should -Be 'string'
+            It 'Should reuse existing Exchange Online session if valid' {
+                # Provide a mock for Get-OrganizationConfig from ExchangeOnlineManagement
+                Mock -CommandName 'Get-OrganizationConfig' -ModuleName 'ExchangeOnlineManagement' -MockWith { @{ Identity = 'TestOrg' } }
+
+                $params = @{
+                    ExchangeOnline = $true
+                    Confirm        = $false
+                }
+                Connect-TkMsService @params
+
+                Assert-MockCalled -CommandName 'Get-OrganizationConfig' -ModuleName 'ExchangeOnlineManagement' -Times 1
+                Assert-MockCalled -CommandName 'Write-AuditLog' -ModuleName 'GraphAppToolkit' -Times 1 `
+                    -ParameterFilter { $_ -eq 'An active Exchange Online session is detected.' }
             }
         }
     }
 }
-
