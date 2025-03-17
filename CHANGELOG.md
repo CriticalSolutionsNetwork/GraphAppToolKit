98854eee87faaa28b5b21d986bc04b3a6ae09103
DrIOS <58635327+DrIOSX@users.noreply.github.com>
2025-03-17 08:56:40 -0500

    removed: Module install in mail app when using manual params

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -20,6 +20,7 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 - Updated private function names to be more descriptive.
 - Removed MSAL.PS dependency from Send-TkEmailAppMessage function.
+- Removed Send-TkEmailAppMessage module install if manual parameters are provided.
 
 ## [0.2.0] - 2025-03-14
 
--- source/Public/Send-TkEmailAppMessage.ps1
+++ source/Public/Send-TkEmailAppMessage.ps1
@@ -183,17 +183,6 @@ function Send-TkEmailAppMessage {
             Write-AuditLog -BeginFunction
         }
         Write-AuditLog '###########################################################'
-        # Install and import the Microsoft.Graph module. Tested: 1.22.0
-        $PublicMods = `
-            'Microsoft.PowerShell.SecretManagement', 'SecretManagement.JustinGrote.CredMan'
-        $PublicVers = `
-            '1.1.2', '1.0.0', '4.37.0.0'
-        $params1 = @{
-            PublicModuleNames      = $PublicMods
-            PublicRequiredVersions = $PublicVers
-            Scope                  = 'CurrentUser'
-        }
-        Initialize-TkModuleEnv @params1
         # If manual parameter set:
         if ($PSCmdlet.ParameterSetName -eq 'Manual') {
             $cert = Get-ChildItem -Path Cert:\CurrentUser\My | Where-Object { $_.Thumbprint -eq $CertThumbprint } -ErrorAction Stop
@@ -207,6 +196,17 @@ function Send-TkEmailAppMessage {
             $Tenant = $TenantId
         }
         elseif ($PSCmdlet.ParameterSetName -eq 'Vault') {
+            # Install and import the Microsoft.Graph module. Tested: 1.22.0
+            $PublicMods = `
+                'Microsoft.PowerShell.SecretManagement', 'SecretManagement.JustinGrote.CredMan'
+            $PublicVers = `
+                '1.1.2', '1.0.0', '4.37.0.0'
+            $params1 = @{
+                PublicModuleNames      = $PublicMods
+                PublicRequiredVersions = $PublicVers
+                Scope                  = 'CurrentUser'
+            }
+            Initialize-TkModuleEnv @params1
             # If a GraphEmailApp object was not passed in, attempt to retrieve it from the local machine
             if ($AppName) {
                 try {
@@ -242,7 +242,7 @@ function Send-TkEmailAppMessage {
     Process {
         # https://learn.microsoft.com/en-us/entra/identity-platform/v2-oauth2-client-creds-grant-flow#second-case-access-token-request-with-a-certificate
         # Authenticate with Azure AD and obtain an access token for the Microsoft Graph API using the certificate
-            $MSToken = Get-TkMsalToken `
+        $MSToken = Get-TkMsalToken `
             -ClientCertificate $Cert `
             -ClientId $AppId `
             -TenantId $Tenant `
